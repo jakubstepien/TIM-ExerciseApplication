6449bf3c96ed94dfad7271a9928c7ac51657807e
Jakub Stępień <qiubon0@gmail.com>
2017-06-26 16:58:16 +0200

    Added traings list view

---
+++ MobileApp/MobileApp/ViewModels/Trainings/TrainingListViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileApp.ViewModels.Trainings
+{
+    class TrainingListViewModel : BaseViewModel
+    {
+        public TrainingListViewModel(IEnumerable<TrainingListItemViewModel> trainings)
+        {
+            Trainings = new ObservableCollection<TrainingListItemViewModel>(trainings);
+        }
+
+        private ObservableCollection<TrainingListItemViewModel> trainings;
+
+        public ObservableCollection<TrainingListItemViewModel> Trainings
+        {
+            get { return trainings; }
+            set { trainings = value; OnPropertyChanged(); }
+        }
+
+        public async Task RemoveById(Guid id)
+        {
+            int index = -1;
+            await Task.Factory.StartNew(() => {
+                try
+                {
+                    index = Trainings.Select((s, i) => new { Index =  i, Value = s }).Where(w => w.Value.Id == id).First().Index;
+                }
+                catch(Exception e) { }
+            });
+            if(index != -1)
+            {
+                Trainings.RemoveAt(index);
+            }
+        }
+    }
+
+    class TrainingListItemViewModel : BaseViewModel
+    {
+        private Guid id;
+        public Guid Id
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged(); }
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(); }
+        }
+
+    }
+}
+
--- MobileApp/MobileApp/Views/Trainings/TrainingsList.xaml.cs
+++ MobileApp/MobileApp/Views/Trainings/TrainingsList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileApp.ViewModels.Trainings;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,31 @@ namespace MobileApp.Views.Trainings
         public TrainingsList()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            TrainingListViewModel viewModel = null;
+            await Task.Factory.StartNew(() =>
+            {
+                viewModel = new TrainingListViewModel(Enumerable.Range(0, 10).Select(s => new TrainingListItemViewModel { Id = Guid.NewGuid(), Name = s.ToString() }));
+            });
+            this.BindingContext = viewModel;
+        }
+
+        private async void DeleteTraining(object sender, EventArgs e)
+        {
+            var id = (sender as Button).CommandParameter;
+            Guid trainingId;
+            if (id != null && Guid.TryParse(id.ToString(), out trainingId))
+            {
+                var action = await DisplayAlert("Usunięcie treningu","Czy napewno chcesz usunąć trening", "Tak", "Nie");
+                if (action)
+                {
+                    var viewModel = BindingContext as TrainingListViewModel;
+                    await viewModel.RemoveById(trainingId);
+                }
+            }
         }
     }
 }
