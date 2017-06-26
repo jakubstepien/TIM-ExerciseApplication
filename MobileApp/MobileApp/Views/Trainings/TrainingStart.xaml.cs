7f084067ca6bc3c55544ad234c113a621be0d056
Jakub Stępień <qiubon0@gmail.com>
2017-06-26 18:21:41 +0200

    Added traing start view

--- MobileApp/MobileApp/Views/Trainings/TrainingStart.xaml.cs
+++ MobileApp/MobileApp/Views/Trainings/TrainingStart.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileApp.Services.Training;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,32 @@ namespace MobileApp.Views.Trainings
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TrainingStart : ContentPage
     {
+        ITrainingService trainingService;
+
         public TrainingStart()
         {
             InitializeComponent();
+        }
+
+        public TrainingStart(ITrainingService trainingService, string trainingName)
+        {
+            Title = trainingName;
+            this.trainingService = trainingService;
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            currentExerciseName.Text = "Pompki";
+            status.Text = "2/5";
+            series.Text = "Seria 1/2";
+            timer.Text = "80";
+            base.OnAppearing();
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+
         }
     }
 }
